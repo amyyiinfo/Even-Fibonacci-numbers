bc00cc472429dff49e3a3c8818c4adc0198ee943
Amy Yi <amyyi.info@gmail.com>
2017-03-14 00:13:06 -0400

    Add files via upload

---
+++ EvenSumFibonacci/EvenSumFibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenSumFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int a = 1;
+                int b = 2;
+                int c = 0;
+                int sum = 2;
+
+                while (b < 4000000)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+
+
+                    if (c % 2 == 0)
+                    {
+                        sum = sum + c;
+                    }
+                }
+                Console.WriteLine(sum);
+                Console.Read();
+        }
+         
+        
+    }
+}
